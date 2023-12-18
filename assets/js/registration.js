0fa55f017a0b4bb647d79f25b7975170ec3bf8dc
johnlestercurro <johnlestercurro@gmail.com>
2023-12-18 11:13:42 +0800

    added details

--- assets/js/registration.js
+++ assets/js/registration.js
@@ -1,74 +1,25 @@
-// Stores user data in local storage
-const localStorageKey = "CCIS-FTRMS-Users";
+document.addEventListener('DOMContentLoaded', function () {
+  // Select the form element
+  var registerForm = document.querySelector('form');
 
-const getUsers = () => {
-  const usersJson = localStorage.getItem(localStorageKey);
-  return usersJson ? JSON.parse(usersJson) : [];
-};
+  // Add a submit event listener to the form
+  registerForm.addEventListener('submit', function (event) {
+    event.preventDefault(); // Prevent the default form submission behavior
 
-const saveUsers = (users) => {
-  localStorage.setItem(localStorageKey, JSON.stringify(users));
-};
+    // Get form data
+    var formData = {
+      name: document.getElementById('yourName').value,
+      email: document.getElementById('yourEmail').value,
+      username: document.getElementById('yourUsername').value,
+      password: document.getElementById('yourPassword').value,
+    };
 
-const addUser = (user) => {
-  const users = getUsers();
-  users.push(user);
-  saveUsers(users);
-};
+    // Store data in local storage
+    localStorage.setItem('userData', JSON.stringify(formData));
 
-const clearUsers = () => {
-  localStorage.removeItem(localStorageKey);
-};
+    // You can perform additional actions here, such as sending the data to a server
 
-// Handles form submission
-const registerForm = document.getElementById("register-form");
-
-registerForm.addEventListener("submit", (event) => {
-  event.preventDefault();
-
-  const name = document.getElementById("yourName").value;
-  const email = document.getElementById("yourEmail").value;
-  const username = document.getElementById("yourUsername").value;
-  const password = document.getElementById("yourPassword").value;
-
-  // Validate form fields
-  if (!name || !email || !username || !password) {
-    alert("Please fill in all required fields.");
-    return;
-  }
-
-  // Check for existing username
-  const existingUsers = getUsers();
-  const existingUser = existingUsers.find((user) => user.username === username);
-  if (existingUser) {
-    alert("Username already exists. Please choose a different one.");
-    return;
-  }
-
-  // Create user object and save to storage
-  const user = {
-    name,
-    email,
-    username,
-    password,
-  };
-  addUser(user);
-
-  // Clear form and show success message
-  registerForm.reset();
-  alert("Account created successfully. Please log in to proceed.");
-
-  // Redirect to login page
-  window.location.href = "pages-login.html";
+    // Optionally, redirect to another page
+    // window.location.href = 'success.html';
+  });
 });
-const registerButton = document.getElementById("register-button");
-
-registerButton.addEventListener("click", event => {
-  registerForm.submit();
-});
-
-
-// Additional functionality (optional)
-// - Implement login logic to check username and password against stored data
-// - Add features like user profile editing and account deletion
-// - Use a secure way to store passwords (e.g., hashing)
